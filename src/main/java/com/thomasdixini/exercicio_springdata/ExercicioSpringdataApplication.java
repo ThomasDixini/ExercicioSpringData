59bfd3e6cec922ec800f7b38d9c1204f0a24ebf5
Thomás Dixini <thomasdixini@gmail.com>
2024-04-23 20:24:53 -0300

    Commit Inicial

---
+++ src/main/java/com/thomasdixini/exercicio_springdata/ExercicioSpringdataApplication.java
@@ -0,0 +1,13 @@
+package com.thomasdixini.exercicio_springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSpringdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioSpringdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomasdixini/exercicio_springdata/ExercicioSpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasdixini.exercicio_springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSpringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
